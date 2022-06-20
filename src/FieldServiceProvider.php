6e5dd83b3f57a0c88f1182dc90f159cbe48b21cc
Lee <lee@few.io>
2022-06-20 14:54:19 -0400

    add migrations and models for comment and suggestion

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -45,17 +45,25 @@ class FieldServiceProvider extends ServiceProvider
         ], 'nova-ckeditor-config');
 
         $this->publishes([
+            # controllers
+            __DIR__ . '/../stubs/controllers/CommentController.stub' => app_path('Http/Controllers/CKEditor/CommentController.php'),
+            __DIR__ . '/../stubs/controllers/SuggestionController.stub' => app_path('Http/Controllers/CKEditor/SuggestionController.php'),
+
             # views
             __DIR__ . '/../stubs/views' => resource_path('views/ckeditor'),
 
             # migrations
             __DIR__ . '/../stubs/migrations/2021_01_01_000000_create_images_table.stub' => database_path('migrations/2021_01_01_000000_create_images_table.php'),
             __DIR__ . '/../stubs/migrations/2021_01_01_000000_create_videos_table.stub' => database_path('migrations/2021_01_01_000000_create_videos_table.php'),
+            __DIR__ . '/../stubs/migrations/2021_01_01_000000_create_comments_table.stub' => database_path('migrations/2021_01_01_000000_create_comments_table.php'),
+            __DIR__ . '/../stubs/migrations/2021_01_01_000000_create_suggestions_table.stub' => database_path('migrations/2021_01_01_000000_create_suggestions_table.php'),
 
             # models
             __DIR__ . '/../stubs/models/Image.stub' => app_path('Models/Image.php'),
             __DIR__ . '/../stubs/models/Video.stub' => app_path('Models/Video.php'),
-
+            __DIR__ . '/../stubs/models/Comment.stub' => app_path('Models/Comment.php'),
+            __DIR__ . '/../stubs/models/Suggestion.stub' => app_path('Models/Suggestion.php'),
+            
             # resources
             __DIR__ . '/../stubs/resources/Image.stub' => app_path('Nova/Resources/Image.php'),
             __DIR__ . '/../stubs/resources/Video.stub' => app_path('Nova/Resources/Video.php')
