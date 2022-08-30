a1c9f3e6030afb1664bd15c330155547f763ac6f
Alex Chalupka <lupka@few.io>
2022-08-29 19:47:23 -0500

    turn off source editing warning

--- resources/js/ckeditor/ckeditor.js
+++ resources/js/ckeditor/ckeditor.js
@@ -140,7 +140,7 @@ export default class CkEditor extends ClassicEditorBase {
             Table,
             TableToolbar,
             HtmlEmbed,
-            SourceEditing,
+            // SourceEditing,
             Indent,
             IndentBlock,
             ElementAddAttributes,
